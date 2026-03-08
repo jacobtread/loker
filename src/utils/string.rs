af07a1c3d3b6b40b44ea58e384d3664e7d37bded
Jacobtread <jacobtread@gmail.com>
2026-03-08 13:06:36 +1300

    test: added tests for join_iter_string

--- src/utils/string.rs
+++ src/utils/string.rs
@@ -1,5 +1,7 @@
 use std::fmt::{Display, Write};
 
+/// Joins the [Display] output of all the values produced by as a string
+/// separated by the `sep` separator
 pub fn join_iter_string<I: Display>(mut iterator: impl Iterator<Item = I>, sep: &str) -> String {
     match iterator.next() {
         None => String::new(),
@@ -14,5 +16,46 @@ pub fn join_iter_string<I: Display>(mut iterator: impl Iterator<Item = I>, sep: 
             });
             result
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::utils::string::join_iter_string;
+
+    /// Tests an empty iterator produces an empty string
+    #[test]
+    fn test_join_empty_string() {
+        let empty: Option<String> = None;
+        let expected = "".to_string();
+        let output = join_iter_string(empty.into_iter(), ",");
+        assert_eq!(output, expected);
+    }
+
+    /// Tests an iterator with a single item does not produce the the separator
+    #[test]
+    fn test_join_string_parts_one() {
+        let one: Option<String> = Some("test".to_string());
+        let expected = "test".to_string();
+        let output = join_iter_string(one.into_iter(), ",");
+        assert_eq!(output, expected);
+    }
+
+    /// Tests an iterator with multiple items produces a single separators
+    #[test]
+    fn test_join_string_parts_two() {
+        let parts: Vec<String> = vec!["test".to_string(), "test2".to_string()];
+        let expected = "test,test2".to_string();
+        let output = join_iter_string(parts.into_iter(), ",");
+        assert_eq!(output, expected);
+    }
+
+    /// Tests an iterator with multiple items produces multiple separators
+    #[test]
+    fn test_join_string_parts_multiple() {
+        let parts: Vec<String> = vec!["test".to_string(), "test2".to_string(), "test3".to_string()];
+        let expected = "test,test2,test3".to_string();
+        let output = join_iter_string(parts.into_iter(), ",");
+        assert_eq!(output, expected);
     }
 }
